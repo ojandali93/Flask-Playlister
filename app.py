c14c92f5dbb2e029681a8f40ee1ae5894335280f
OmarJ <omarjandali93@gmail.com>
2021-11-02 15:58:54 -0700

    Users can see comments

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -43,7 +44,8 @@ def playlists_submit():
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -62,7 +64,6 @@ def playlists_update(playlist_id):
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     # Add the title parameter here
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
@@ -71,6 +72,19 @@ def playlists_edit(playlist_id):
 def playlists_delete(playlist_id):
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    return 'playlists comment'
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    comment = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+    }
+    comments.insert_one(comment)
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
